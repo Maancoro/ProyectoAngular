19de98a4c07734c8d235d31f4cdba73c4ec6725c
Angelica <correa_angelica@hotmail.com>
2022-11-24 21:58:12 -0300

    tratando de activar Json

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../servicios/datos.service';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+ //Crear variable de instancia para almacenar los datos con los que trata el Servicio
+  infoPortfolio: any;
 
-  constructor() { }
+  constructor(
+    //Inyectar el servicio para tener acceso en la clase a los Metodos
+    private datosService: DatosService ) { }
 
   ngOnInit(): void {
+    //Esto es almacenar en la variable de instancia los datos recuperados por el servicio?
+    this.datosService.getDatos().subscribe(data =>{
+     console.log(data);
+     this.infoPortfolio=data.nombre;
+    });
   }
 
 }
--- src/app/servicios/datos.service.ts
+++ src/app/servicios/datos.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+//Esto es para subcribirse y que reciba respuesta de forma asincrona
+import { Observable } from 'rxjs';
+//Esto es para poder hacer las peticiones y CRUD
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatosService {
 
-  constructor() { }
+// El primer http se llama ALIAS
+  constructor(private http:HttpClient) { }
+
+
+
+  //Metodo Observable que devuelve datos
+    getDatos():Observable<any>{
+    //retorno de datos utilizando el método get ed HttpClient que llama a la base de datos JSON o a una URL
+    return this.http.get('./assets/db/datos.json');
+    //acá podria poner un callback para ver la opción de problema de conexión del servidor
+  }
+
+
 }
