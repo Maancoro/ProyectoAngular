10fa0c5a79a2f3942d6a7fe5d881179f61b2b80f
Angelica <correa_angelica@hotmail.com>
2022-11-25 21:55:18 -0300

    sigo probando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule}from '@angular/common/http';
 
-import { DatosService  } from './servicios/datos.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -102,7 +102,7 @@ import { Pagina404Component } from './pagina404/pagina404.component';
     HttpClientModule
 
   ],
-  providers: [DatosService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
